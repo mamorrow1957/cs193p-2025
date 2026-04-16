3f840696ba8268f0d88aec96fbaa9170a1b35eb2
Michael Morrow <michael@mamorrow.com>
2026-04-16 17:23:26 -0400

    Initial apple-created code

---
+++ CodeBreaker/CodeBreaker/CodeBreakerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodeBreakerApp.swift
+//  CodeBreaker
+//
+//  Created by michael on 4/16/26.
+//
+
+import SwiftUI
+
+@main
+struct CodeBreakerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
